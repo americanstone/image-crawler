693be663ff525a35240e0bcd7d5674b4575dde9c
Gordon zhang <gordon.zhang@coxautoinc.com>
2021-10-15 16:29:55 -0400

    init

---
+++ src/main/java/edu/vanderbilt/imagecrawler/Application.java
@@ -0,0 +1,36 @@
+package edu.vanderbilt.imagecrawler;
+
+import edu.vanderbilt.imagecrawler.crawlers.CrawlerType;
+import edu.vanderbilt.imagecrawler.crawlers.ImageCrawler;
+import edu.vanderbilt.imagecrawler.platform.Controller;
+import edu.vanderbilt.imagecrawler.platform.JavaPlatform;
+import edu.vanderbilt.imagecrawler.platform.Platform;
+import edu.vanderbilt.imagecrawler.transforms.Transform;
+import edu.vanderbilt.imagecrawler.utils.Options;
+import edu.vanderbilt.imagecrawler.utils.UriUtils;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Application {
+
+    public static void main(String[] args) {
+        boolean local = false;
+        String rootUrl = Options.DEFAULT_WEB_URL;
+        if (local) {
+            rootUrl = Platform.RESOURCES_URI_PREFIX + "/" + UriUtils.mapUriToRelativePath(Options.DEFAULT_WEB_URL);
+        }
+        System.out.println("done?");
+        Controller controller = Controller
+                .newBuilder()
+                .platform(new JavaPlatform())
+                .transforms(Arrays.stream(Transform.Type.values()).collect(Collectors.toList()))
+                .rootUrl(rootUrl)
+                .localTransforms(true)
+                .maxDepth(3)
+                .diagnosticsEnabled(false)
+                .build();
+
+        ImageCrawler.Factory.newCrawler(CrawlerType.SEQUENTIAL_LOOPS, controller).run();
+    }
+}
---
+++ src/main/java/edu/vanderbilt/imagecrawler/utils/ArraySpliterator.java
@@ -0,0 +1,89 @@
+package edu.vanderbilt.imagecrawler.utils;
+
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.Consumer;
+
+/**
+ * Defines an object for traversing and partitioning
+ * elements of an Array.
+ */
+public final class ArraySpliterator<E>
+        extends Spliterators.AbstractSpliterator<E> {
+    /**
+     * The array to traverse and/or partition.
+     */
+    // TODO (graduates 1b and undergraduates 2a) - you fill in here.
+    private Array<E> array;
+    /**
+     * Current index, modified on advance/split.
+     */
+    // TODO (graduates 1b and undergraduates 2a) - you fill in here.
+    private int index;
+    /**
+     * One past the end of the spliterator range.
+     */
+    // TODO (graduates 1b and undergraduates 2a) - you fill in here.
+    private int fence;
+    /**
+     * Create new spliterator covering the given range.
+     */
+    ArraySpliterator(Array<E> array,
+                     int origin,
+                     int end) {
+        super(array.size(),
+                Spliterator.ORDERED | Spliterator.SIZED | Spliterator.SUBSIZED);
+
+        // TODO (graduates 1b and undergraduates 2a) - you fill in here.
+        this.array = array;
+        this.index = origin;
+        this.fence = end;
+    }
+
+    /**
+     * If a remaining element exists, performs the given action on
+     * it, returning true; else returns false.  Throw
+     * NullPointerException of {@code action} is null.
+     */
+    public boolean tryAdvance(Consumer<? super E> action) {
+        // TODO (graduates 1b and undergraduates 2a)
+        //  you fill in here replacing this statement with your solution.
+        if (action == null){
+            throw new NullPointerException();
+        }
+        int hi = getFence(), i = index;
+        if(i < hi){
+            index = i + 1;
+            E e = (E)array.uncheckedToArray()[i];
+            action.accept(e);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * Returns a Spliterator covering elements that will, upon
+     * return from this method, not be covered by this Spliterator.
+     */
+    public ArraySpliterator<E> trySplit() {
+        // TODO (graduates 1b and undergraduates 2a)
+        //  you fill in here replacing this statement with your solution.
+        int hi = getFence(), lo = index, mid = (lo + hi) >>>1;
+        return (lo >= mid) ? null :
+                new ArraySpliterator<>(array, lo, index = mid);// updating the index so the original spliterator
+        // only cover the second half
+    }
+    private int getFence() { // initialize fence to size on first use
+        int hi; // (a specialized variant appears in method forEach)
+        Array<E> lst;
+        if ((hi = fence) < 0) {
+            if ((lst = array) == null)
+                hi = fence = 0;
+            else {
+                hi = fence = lst.size();
+            }
+        }
+        return hi;
+    }
+}
